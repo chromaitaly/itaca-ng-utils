206986c3fa267d4b1118744772fa807080a41d61
scoletti <simone.coletti@chromaitaly.com>
2017-11-07 19:19:21 +0100

    inizio utilities

---
+++ src/js/contact-utils/contact-utils.service.js
@@ -0,0 +1,33 @@
+/**
+ * Restituisce l'href in base il tipo di contatto
+ */
+(function() {
+	'use strict';
+	
+	angular.module("chroma.utils").factory('ContactUtils', ContactUtilsFactory);
+	
+	/* @ngInject */
+	function ContactUtilsFactory() {		
+		var service = {};
+		
+		service.getUri = function(type, value){
+			var uri = '';
+			
+			switch(type){
+				case 'PHONE': uri = 'tel:'; break; 
+				case 'MOBILE': uri = 'tel:'; break; 
+				case 'FAX': uri = 'tel:'; break; 
+				case 'MAIL': uri = 'mailto:'; break; 
+				case 'WEBSITE': uri = 'http://'; break; 
+				case 'FACEBOOK': uri = 'https://www.facebook.com/'; break; 
+				case 'TWITTER': uri = 'https://www.twitter.com/'; break; 
+				case 'INSTAGRAM': uri = 'https://www.instagram.com/'; break; 
+				default: uri = 'http://';
+			}
+			
+			return uri + value ;
+		};
+		
+		return service;
+	}
+})();
