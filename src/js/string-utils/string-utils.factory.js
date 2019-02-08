b4b484e904c25a7f0f733cc9e99a9153ea01431d
Simone Coletti <simone.coletti@chromaitaly.com>
2019-02-08 15:50:58 +0100

    agg bower modules

--- src/js/string-utils/string-utils.factory.js
+++ src/js/string-utils/string-utils.factory.js
@@ -9,28 +9,46 @@
 	
 	/* @ngInject */
 	function StringUtilsFactory(){
-		var service = {};
+		var $$service = {};
 		
-		service.isBlank = function(string) {
+		$$service.isBlank = function(string) {
 			return _.isUndefined(string) || _.isNull(string) || _.isEmpty(string);
 		};
 		
-		service.isNotBlank = function(string) {
-			return !service.isBlank(string);
+		$$service.isNotBlank = function(string) {
+			return !$$service.isBlank(string);
 		};
 		
-		service.isEmpty = function(string) {
-			return service.isNotBlank(string) && _.isEmpty(_.trim(string));
+		$$service.isEmpty = function(string) {
+			return $$service.isNotBlank(string) && _.isEmpty(_.trim(string));
 		};
 		
-		service.isNotEmpty = function(string) {
-			return !service.isEmpty(string);
+		$$service.isNotEmpty = function(string) {
+			return !$$service.isEmpty(string);
 		};
 		
-		service.normalizeForUrl = function(string, useDelimiters) {
+		$$service.normalizeForUrl = function(string, useDelimiters) {
 			return _.toLower(_.replace(_.deburr(useDelimiters ? _.snakeCase(string) : string), /[^\w]/gi, ''));
 		};
 		
-		return service;
+		$$service.isBoolean = function(string) {
+			try {
+				return _.isBoolean(JSON.parse(string));
+			
+			} catch (e) {
+				return false;
+			}
+		};
+		
+		$$service.toBoolean = function(string) {
+			if ($$service.isBoolean(string)) {
+				return JSON.parse(string);
+				
+			} else {
+				return null;
+			}
+		};
+		
+		return $$service;
 	}
 })();
